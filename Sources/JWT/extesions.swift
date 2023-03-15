15b5f5e53f97683305269fb25e7df78f223374b3
Rockford Wei <rocky@perfect.org>
2023-03-15 09:20:47 -0400

    formulate

--- Sources/JWT/extesions.swift
+++ Sources/JWT/extesions.swift
@@ -8,23 +8,17 @@
 import Foundation
 
 public extension UInt32 {
-    static func unpack(from bigEndianBytes: [UInt8], offset: Int) -> Self {
-        return bigEndianBytes[offset..<offset + 4]
-            .enumerated()
-            .map { Self($0.element) << (24 - 8 * $0.offset) }
-            .reduce(0) { $0 | $1 }
+    var gamma0: Self {
+        return xorRightShits(2, 13, 22)
+    }
+    var gamma1: Self {
+        return xorRightShits(6, 11, 25)
     }
     var sigma0: Self {
         return xorRightShits(7, 18) ^ (self >> 3)
     }
     var sigma1: Self {
         return xorRightShits(17, 19) ^ (self >> 10)
-    }
-    var gamma0: Self {
-        return xorRightShits(2, 13, 22)
-    }
-    var gamma1: Self {
-        return xorRightShits(6, 11, 25)
     }
     internal func xorRightShits(_ shifts: Int ...) -> Self {
         return shifts.map { rotateRight(by: $0) }.reduce(0) { $0 ^ $1 }
@@ -41,18 +35,15 @@ public extension UInt32 {
     var bigEndianBytes: [UInt8] {
         return (0..<4).map { UInt8((self >> (24 - 8 * $0)) & 0xFF) }
     }
+    static func unpack(from bigEndianBytes: [UInt8], offset: Int) -> Self {
+        return bigEndianBytes[offset..<offset + 4]
+            .enumerated()
+            .map { Self($0.element) << (24 - 8 * $0.offset) }
+            .reduce(0) { $0 | $1 }
+    }
 }
 
 public extension UInt64 {
-    var bigEndianBytes: [UInt8] {
-        return (0..<8).map { UInt8((self >> (56 - 8 * $0)) & 0xFF) }
-    }
-    func rotateRight(by shift: Int) -> Self {
-        return (self >> shift) | (self << (64 - shift))
-    }
-    internal func xorRightShits(_ shifts: Int ...) -> Self {
-        return shifts.map { rotateRight(by: $0) }.reduce(0) { $0 ^ $1 }
-    }
     var gamma0: Self {
         return xorRightShits(28, 34, 29)
     }
@@ -64,6 +55,27 @@ public extension UInt64 {
     }
     var sigma1: Self {
         return xorRightShits(19, 61) ^ (self >> 6)
+    }
+    internal func xorRightShits(_ shifts: Int ...) -> Self {
+        return shifts.map { rotateRight(by: $0) }.reduce(0) { $0 ^ $1 }
+    }
+    func rotateRight(by shift: Int) -> Self {
+        return (self >> shift) | (self << (64 - shift))
+    }
+    static func choose(x: Self, y: Self, z: Self) -> Self {
+        return (x & y) ^ (~x & z)
+    }
+    static func major(x: Self, y: Self, z: Self) -> Self {
+        return (x & y) ^ (x & z) ^ (y & z)
+    }
+    var bigEndianBytes: [UInt8] {
+        return (0..<8).map { UInt8((self >> (56 - 8 * $0)) & 0xFF) }
+    }
+    static func unpack(from bigEndianBytes: [UInt8], offset: Int) -> Self {
+        return bigEndianBytes[offset..<offset + 8]
+            .enumerated()
+            .map { Self($0.element) << (56 - 8 * $0.offset) }
+            .reduce(0) { $0 | $1 }
     }
 }
 
